61e1a2a250ffe26abfd5b4ae33d9440bb5808f66
polluterofminds <justin.edward.hunter@gmail.com>
2023-02-13 17:19:47 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Static-Marketing`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
